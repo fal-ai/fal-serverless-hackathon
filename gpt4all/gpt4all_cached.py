2aa6ce1f163bac26cb2a7605bf7d61576004c96b
Burkay Gur <burkaygur@gmail.com>
2023-05-05 11:37:01 -0400

    Hackathon example

---
+++ gpt4all/gpt4all_cached.py
@@ -0,0 +1,37 @@
+from fal_serverless import isolated, cached
+
+
+# Run the model in an isolated environment
+@isolated(
+    "virtualenv",
+    requirements=["git+https://github.com/nomic-ai/pygpt4all", "huggingface_hub"],
+    machine_type="L",
+    keep_alive=10,  # Keep alive for 10 seconds
+)
+def run_basic_gpt(prompt: str) -> str:
+    model = gpt()
+    return "".join(
+        model.generate(prompt, n_predict=10)
+    )  # n_predict is the max of tokens to generate
+
+
+# Cache the model in memory during keep alive to save time
+@cached
+def gpt():
+    import os
+
+    os.environ["XDG_CACHE_HOME"] = "/data/.cache"
+
+    from huggingface_hub import hf_hub_download
+    from pygpt4all.models.gpt4all import GPT4All
+
+    data_file = hf_hub_download(
+        repo_id="LLukas22/gpt4all-lora-quantized-ggjt",
+        filename="ggjt-model.bin",
+    )
+
+    model = GPT4All(data_file)
+    return model
+
+
+print(run_basic_gpt("Explain how a compiler works in layman's terms."))
---
+++ gpt4all/gpt4all_simple.py
@@ -0,0 +1,28 @@
+from fal_serverless import isolated
+
+
+# Run the model in an isolated environment
+@isolated(
+    "virtualenv",
+    requirements=["git+https://github.com/nomic-ai/pygpt4all", "huggingface_hub"],
+    machine_type="L",
+    keep_alive=10,  # Keep alive for 10 seconds
+)
+def run_basic_gpt(prompt: str) -> str:
+    import os
+
+    os.environ["XDG_CACHE_HOME"] = "/data/.cache"
+
+    from huggingface_hub import hf_hub_download
+    from pygpt4all.models.gpt4all import GPT4All
+
+    data_file = hf_hub_download(
+        repo_id="LLukas22/gpt4all-lora-quantized-ggjt",
+        filename="ggjt-model.bin",
+    )
+
+    model = GPT4All(data_file)
+    return "".join(model.generate(prompt, n_predict=10))
+
+
+print(run_basic_gpt("Explain how a compiler works in layman's terms."))
